8761d8791eb831bc16cf37b97a2c058edf2dd329
Marcelo José <55902016+Marcelo-D2@users.noreply.github.com>
2024-02-19 10:10:05 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,76 @@
+let textoEntrada = "";
+let textoSaida = "";
+let textoBotaoCopiar = "";
+const regex = "[^a-z ]";
+
+document.getElementById('resultado_descripto').style.display = 'none';
+
+function criptografar() {
+    let resultado = document.getElementById('resultado');
+    textoEntrada = document.querySelector('textarea').value;
+
+    textoSaida = textoEntrada.replace(/e/g, "zen")
+                                .replace(/i/g, "ni")
+                                .replace(/a/g, "san")
+                                .replace(/o/g, "yon")
+                                .replace(/u/g, "kyu");
+
+    textoBotaoCopiar = textoSaida;
+    resultado.innerHTML = textoSaida;
+
+    document.getElementById('resultado_cripto').style.display = 'none';
+    document.getElementById('resultado_descripto').style.display = 'block';
+}
+
+function descriptografar() {
+    let resultado = document.getElementById('resultado');
+    textoEntrada = document.querySelector('textarea').value;
+
+    textoSaida = textoEntrada.replace(/enter/g, "e")
+                                .replace(/imes/g, "i")
+                                .replace(/ai/g, "a")
+                                .replace(/ober/g, "o")
+                                .replace(/ufat/g, "u");
+
+    textoBotaoCopiar = textoSaida;
+    resultado.innerHTML = textoSaida;
+
+    document.getElementById('resultado_cripto').style.display = 'none';
+    document.getElementById('resultado_descripto').style.display = 'block';
+}
+
+function copiar() {
+    navigator.clipboard.writeText(textoBotaoCopiar);
+}
+
+function validarEntradaCrip(){
+    textoEntrada = document.querySelector('textarea').value;
+ 
+    if (!textoEntrada.match(regex)) {
+        document.getElementById('textoAviso').style.color = '#00d4ff'; 
+        document.getElementById('textoAviso').style.fontSize= '15px';            
+        criptografar();   
+    } else {
+        document.getElementById('resultado').innerHTML = "";
+        document.getElementById('resultado_cripto').style.display = 'block';
+        document.getElementById('resultado_descripto').style.display = 'none';
+        document.getElementById('textoAviso').style.color = '#00d4ff';
+        document.getElementById('textoAviso').style.fontSize= '25px';    
+    }
+}
+
+function validarEntradaDescrip() {
+    textoEntrada = document.querySelector('textarea').value;
+ 
+    if (!textoEntrada.match(regex)) {
+        document.getElementById('textoAviso').style.color = '#00d4ff'; 
+        document.getElementById('textoAviso').style.fontSize= '15px';    
+       descriptografar();
+    } else {
+        document.getElementById('resultado').innerHTML = "";
+        document.getElementById('resultado_cripto').style.display = 'block';
+        document.getElementById('resultado_descripto').style.display = 'none';
+        document.getElementById('textoAviso').style.color = '#00d4ff'; 
+        document.getElementById('textoAviso').style.fontSize= '25px';            
+    }
+}
